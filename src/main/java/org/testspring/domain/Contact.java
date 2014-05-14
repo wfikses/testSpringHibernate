957178a7a124a0c9935c0a0960b8eaff4d5aabe2
wfikses <w.fikses@gmail.com>
2014-05-14 18:38:08 +0300

    First commit

---
+++ src/main/java/org/testspring/domain/Contact.java
@@ -0,0 +1,73 @@
+package org.testspring.domain;
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by admin on 14.05.2014.
+ */
+@Entity
+@Table(name = "contact")
+public class Contact implements Serializable {
+    private long id;
+    private int version;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Version
+    @Column(name = "version")
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "birth_date")
+    @Temporal(TemporalType.DATE)
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String toString() {
+        return "Contact - Id: " + id + ", First name: " + firstName
+                + ", Last name: " + lastName + ", Birthday: " + birthDate;
+    }
+}
